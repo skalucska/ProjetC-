4f33d4670f1e6f30ef99c73b0f4cd06a634863dc
Julien <juju.dupale@gmail.com>
2022-01-19 11:10:32 +0100

    error commit

---
+++ GestionEmploye.EFcoreDemo/EmployeContext.cs
@@ -0,0 +1,21 @@
+﻿using GestionEmploye.Common;
+using GestionEmploye.Common.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace GestionEmploye.EFcoreDemo
+{
+    public class EmployeContext : DbContext
+    {
+        public DbSet<Employe> Employes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Date Source= (local)\\MSSQLLocalDB; Initial Catalog=GestionEmploye").LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information).EnableSensitiveDataLogging();
+        }
+
+    }
+
+}
--- GestionEmploye.WPF/obj/Debug/net5.0-windows/App.g.i.cs
+++ GestionEmploye.WPF/obj/Debug/net5.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace GestionEmploye.WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace GestionEmploye.WPF {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/GestionEmploye.WPF;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GestionEmploye.WPF;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace GestionEmploye.WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             GestionEmploye.WPF.App app = new GestionEmploye.WPF.App();
             app.InitializeComponent();
